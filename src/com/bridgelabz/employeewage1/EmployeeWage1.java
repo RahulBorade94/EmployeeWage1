ad10c0e1a1ae797280ff427d37e6e1c014bbf353
RahulBorade94 <boraderahul94@gmail.com>
2021-12-30 19:30:52 +0530

    MODIFIED: Employee Daily Wage Calculated

--- src/com/bridgelabz/employeewage1/EmployeeWage1.java
+++ src/com/bridgelabz/employeewage1/EmployeeWage1.java
@@ -1,12 +1,25 @@
 package com.bridgelabz.employeewage1;
 
 public class EmployeeWage1 {
+    private static int WAGE_PER_HOUR = 20;
+    private static int IS_PART_TIME = 1;
+    private static int IS_FULL_TIME = 2;
     public static void main(String[] args) {
-        int IS_PRESENT =1;
-        int empCheck =(int)Math.floor(Math.random() *10 )%2;
-        if(empCheck == IS_PRESENT)
-            System.out.println("Employee is Prsent");
-        else
+
+        int empCheck =(int) Math.floor(Math.random() *10 )%3;
+
+        int empWage = 0;
+        int empHrs = 8;
+
+        if ( empCheck == IS_PART_TIME ) {
+            empHrs = 4;
+        } else if ( empCheck == IS_FULL_TIME ) {
+            empHrs = 8;
+        }else {
             System.out.println("Employee is Absent");
+
+        }
+        empWage = WAGE_PER_HOUR * empHrs ;
+        System.out.println("Employee Wage : "+empWage);
     }
 }
