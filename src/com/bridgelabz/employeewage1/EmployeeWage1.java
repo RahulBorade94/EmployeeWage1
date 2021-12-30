1ab1790b69d036c3a949d9e4d255b5326c18cf20
RahulBorade94 <boraderahul94@gmail.com>
2021-12-30 21:56:19 +0530

    MODIFIED: Calculated Wages on Time Basis

--- src/com/bridgelabz/employeewage1/EmployeeWage1.java
+++ src/com/bridgelabz/employeewage1/EmployeeWage1.java
@@ -1,25 +1,28 @@
 package com.bridgelabz.employeewage1;
 
 public class EmployeeWage1 {
-    private static int WAGE_PER_HOUR = 20;
+
     private static int IS_PART_TIME = 1;
     private static int IS_FULL_TIME = 2;
+    private static int EMP_RATE_PER_HOUR = 20;
     public static void main(String[] args) {
 
         int empCheck =(int) Math.floor(Math.random() *10 )%3;
 
         int empWage = 0;
-        int empHrs = 8;
+        int empHrs = 0;
 
         if ( empCheck == IS_PART_TIME ) {
             empHrs = 4;
         } else if ( empCheck == IS_FULL_TIME ) {
             empHrs = 8;
         }else {
-            System.out.println("Employee is Absent");
+            empHrs=0;
+
 
         }
-        empWage = WAGE_PER_HOUR * empHrs ;
+
+        empWage = empHrs * EMP_RATE_PER_HOUR ;
         System.out.println("Employee Wage : "+empWage);
     }
 }
